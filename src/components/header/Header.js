b0bb01fdb26f8d33dc24aaf02d338b7064d9c339
arijeet <arijeetsinha2@gmail.com>
2023-09-04 12:58:06 +0530

    fixed cart count

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -43,7 +43,7 @@ function Header() {
       } catch {
         console.log("error while cart counting");
       }
-    }, [cartItems]);
+    }, [cartItems,User]);
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged((user) => {
